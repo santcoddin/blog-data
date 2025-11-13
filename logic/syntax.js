5554ac487aea055619d35ec3c9da38f10dd7a36f
santcoddin <mmxixsantoro@gmail.com>
2025-11-13 00:22:41 -0300

    Update syntax.js

--- logic/syntax.js
+++ logic/syntax.js
@@ -1,1 +1,22 @@
-
+/*
+  syntax, estou começando a ficar com sono...
+  - root: operador ou predicado
+  - first: primeiro argumento
+  - second: segundo argumento
+*/
+class Formula {
+  constructor(root, first = null, second = null) {
+    this.root = root;
+    this.first = first;
+    this.second= second;
+  }
+toString() {
+  if (!this.first && !this.second) return this.root;
+  if (this.first && !this.second) return `(${this.root}${this.first})`;
+  return `(${this.first}${this.root}${this.second})`;
+  }
+}
+const p = new Formula('p');
+const q = new Formula('q');
+const imp = new Formula('→', p, q);
+console.log(imp.toString());
